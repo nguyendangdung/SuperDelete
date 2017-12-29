fd93872aa803ab75a8e90a4e4aafb28f6df0d54e
Dan Nguyen <ddnguyen0702@hotmail.com>
2017-12-30 03:53:55 +0700

    update

--- SuperDelete/DeleteHelper.cs
+++ SuperDelete/DeleteHelper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace SuperDelete
 {
@@ -15,8 +16,10 @@ namespace SuperDelete
             {
                 var deleteFilesCmd = $"/C del /f /s /q {dir} > nil";
                 var deleteFoldersCmd = $"/C rmdir /s /q {dir}";
-                System.Diagnostics.Process.Start("CMD.exe", deleteFilesCmd);
-                System.Diagnostics.Process.Start("CMD.exe", deleteFoldersCmd);
+                Interaction.Shell(deleteFilesCmd, AppWinStyle.Hide);
+                Interaction.Shell(deleteFoldersCmd, AppWinStyle.Hide);
+                //System.Diagnostics.Process.Start("CMD.exe", deleteFilesCmd);
+                //System.Diagnostics.Process.Start("CMD.exe", deleteFoldersCmd);
             }
             catch (Exception e)
             {
